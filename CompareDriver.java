2185937b9bf6f830ffed9b6a0e6e0aba9853ffc1
FoundedDragon98 <31353323+FoundedDragon98@users.noreply.github.com>
2017-12-01 10:50:44 -0800

    12/1/17

---
+++ CompareDriver.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class CompareDriver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CompareDriver
+{
+    public static void main(String[] args) {
+        String st1 = new String("hi");
+        String st2 = new String("bye");
+        
+        System.out.println(Compare1.largest(st1,st2));
+        System.out.println(Compare3.largest(st1,st2));
+        System.out.println(Compare1.largest(i1,i2));
+    }
+}
